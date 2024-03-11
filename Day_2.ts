4ee8ca7a880136f46556c6b41e1765a7a0e1f5dd
Rooh-U-Din <160412491+Rooh-U-Din@users.noreply.github.com>
2024-03-11 04:25:20 -0700

    Add files via upload
    100-Days challenge Day_2
    Typescript code

---
+++ Day_2.ts
@@ -0,0 +1,16 @@
+// //Question No:1: Famous Quote:
+// const quote:string=("Allama Muhammad Iqbal once said,Be aware of your own worth,\nuse all of your power to achieve it.\nCreate an ocean from a dewdrop.\nDo not beg for light from the moon,\nobtain it from the spark within you.")
+// console.log(quote)
+
+// //Question No:2: Famous Quote 2:
+
+// let Name = "Allama Muhammad Iqbal"
+// const quote_repeat:string=(`${Name}Be aware of your own worth,\nuse all of your power to achieve it.\nCreate an ocean from a dewdrop.\nDo not beg for light from the moon,\nobtain it from the spark within you.`)
+
+// console.log(quote_repeat)
+
+// //Question No:3:Stripping:
+// const Strip_Name = ("Allama\tmuhammad\nIqbal")
+// console.log(Strip_Name)
+
+// //Day 2 Complete.
