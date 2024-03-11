816a9a6dc67955c2dbfae25744b512cd4d088601
Rooh-U-Din <160412491+Rooh-U-Din@users.noreply.github.com>
2024-03-11 04:20:55 -0700

    Add files via upload
    100-Days challenge Day-1 assignments 
    
    Typescript cod file

---
+++ Day_1.ts
@@ -0,0 +1,20 @@
+// //Question No:1
+// let Name = "Fida"
+// console.log(`Hello ${Name} do you want to learn Typescript`)
+
+// //Question No:2
+
+// let Full_Name = "FiDa"
+// console.log(`UpperCase: ${Full_Name.toUpperCase()}` )
+// console.log(`LowerCase: ${Full_Name.toLowerCase()}`)
+
+// const Title_Case =(person_name:string)=>{
+//     let name;
+//     name = person_name.toLowerCase().split(" ")
+//     for(let i = 0;i < name.length;i++){
+//         name[i]= name[i].charAt(0).toUpperCase() + name[i].slice(1);
+//     }
+//     return name.join (" ");
+//  }
+//  console.log(`titleCase: ${Title_Case("FIDA")}`);
+// // Day 1 Complete.
