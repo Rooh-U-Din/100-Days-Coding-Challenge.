c5a7adfd0f0634784069b7221dbeee57a1d488e4
Rooh-U-Din <160412491+Rooh-U-Din@users.noreply.github.com>
2024-03-11 04:34:25 -0700

    100-Days-Coding-Challenge Day-8
    100-Days-Coding-Challenge Day-8

---
+++ Day_8.ts
@@ -0,0 +1,57 @@
+// // Question_1: Error
+// let languages=["HTML","CSS","Typescript"]
+// console.log(languages[3])// error nothing on index 3.
+
+// console.log(languages[2] = "Typescript") // now correct error accessing  a valid index.
+
+// // Question_2: Conditional tests
+
+// const game = "Tekken 7"
+// if (game === "Tekken 7") {
+//     console.log("i love to play Tekken 7")
+// }else{
+//     console.log("Tekken 7 is not available");
+    
+// }
+
+// // Question_3: More Conditional tests
+
+// const age: number = 25;
+// const isStudent: boolean = false;
+// const color: string = "Black";
+// const fruits: string[] = ["apple", "banana", "orange"];
+// const person: { name: string; age: number } = { name: "Fida", age: 30 };
+
+// if (age >= 18) {
+//   console.log("You are an adult.");
+// } else {
+//   console.log("You are a minor.");
+// }
+
+// if (isStudent) {
+//   console.log("You are a student.");
+// } else {
+//   console.log("You are not a student.");
+// }
+
+// if (color === "red") {
+//   console.log("The color is red.");
+// } else if (color === "blue") {
+//   console.log("The color is blue.");
+// } else {
+//   console.log("The color is neither red nor blue.");
+// }
+
+// if (fruits.length > 5) {
+//   console.log("You have many fruits.");
+// } else if (fruits.length > 0) {
+//   console.log("You have some fruits.");
+// } else {
+//   console.log("You have no fruits.");
+// }
+
+// if (person.age < 18 || person.name !== "Fida") {
+//   console.log("You are either under 18 or not named John.");
+// } else {
+//   console.log("You are over 18 and your name is Fida.");
+// }
