6521692c86c309e16a18c593f49e9e2a465ea6f2
Rooh-U-Din <160412491+Rooh-U-Din@users.noreply.github.com>
2024-03-11 04:31:53 -0700

    100-Days-Coding-Challenge Day-4
    100-Days-Coding-Challenge Day-4

---
+++ Day_4.ts
@@ -0,0 +1,21 @@
+// // Question_1: Favorite Number
+//  //Name [ROOH U DIN]
+//  //date [saturday; 3/9/2024]
+//  // this code shows my favorite number.
+// let Favorite_Number :number=(4)
+// console.log(`My favorite number is ${Favorite_Number}`)
+
+// //Question_2:Names
+// const Friends_Names =["Thanos","Thor","Wanda","Buckey"]
+// console.log(Friends_Names[0]);
+// console.log(Friends_Names[1]);
+// console.log(Friends_Names[2]);
+// console.log(Friends_Names[3]);
+// //Question_3:Greetings
+// const Greetings =["Thanos","Thor","Wanda","Buckey"]
+// console.log(Greetings[0],"my Day_4 is completed.");
+// console.log(Greetings[1],"my Day_4 is completed.");
+// console.log(Greetings[2],"my Day_4 is completed.");
+// console.log(Greetings[3],"my Day_4 is completed.");
+
+
