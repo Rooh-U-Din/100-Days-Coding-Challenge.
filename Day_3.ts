42633fc9f9819ae3c0bb0c2496a1b3bc3c1cb5fc
Rooh-U-Din <160412491+Rooh-U-Din@users.noreply.github.com>
2024-03-11 04:31:20 -0700

    Add files via upload
    100-Days-Coding-Challenge Day-3

---
+++ Day_3.ts
@@ -0,0 +1,23 @@
+// // Question_1: Number Eight
+// //Addition
+// console.log(2+3+3)
+// //subtraction
+// console.log(100-23-69)
+// //multiplication
+// console.log(2*4)
+// //division
+// console.log(64/4/2)
+// //modulo
+// console.log(80%9)
+
+// // Question_2
+// console.log(2+3+3)
+// console.log(100-23-69)
+// console.log(2*4)
+// console.log(64/4/2)
+
+// // Question_3: Favorite Number
+// let Favorite_Number :number=(4)
+// console.log(`My favorite number is ${Favorite_Number}`)
+
+// //Day_3 Completed.
