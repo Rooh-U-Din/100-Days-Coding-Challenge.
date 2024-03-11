c157d19be15be3fb5ab73d954b96ec73b2d131a7
Rooh-U-Din <160412491+Rooh-U-Din@users.noreply.github.com>
2024-03-11 04:33:02 -0700

    100-Days-Coding-Challenge Day-5
    100-Days-Coding-Challenge Day-5

---
+++ Day_5.ts
@@ -0,0 +1,24 @@
+// //Question_1:Transport
+// let Transportation=["Motorcycle","Lamborghini","Helicopter"]
+// Transportation.forEach(Transportation => {
+//     console.log(`I would like to own ${Transportation}`)
+// })
+
+// //Question_2:Guest list
+
+// let Guest_list=["Thanos","Thor","Wanda","Buckey"]
+// Guest_list.forEach(Guest_list =>{
+//     console.log(`Hey ${Guest_list} would you like  dinner with me.`)
+// })
+
+// //Question_3:Replace Guest:
+
+// let Busy ="Thanos"
+// console.log(`${Busy} is busy to find infinity stones.`)
+
+// let New_Guest = "Batman"
+// Guest_list[Guest_list.indexOf(Busy)]=New_Guest
+// Guest_list.forEach(Guest_list => {
+//     console.log(`Hey ${Guest_list} would you like  dinner with me.`);
+// });
+
