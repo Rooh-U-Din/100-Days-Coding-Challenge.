6a8047f28f662831e301394f118c1aaf833d93e7
Rooh-U-Din <160412491+Rooh-U-Din@users.noreply.github.com>
2024-03-11 04:33:36 -0700

    100-Days-Coding-Challenge Day-6
    100-Days-Coding-Challenge Day-6

---
+++ Day_6.ts
@@ -0,0 +1,34 @@
+// // Question_1: More Guests.
+// let Guest_list=["BatMan","Thor","Wanda","Buckey"]
+// Guest_list.unshift("SpiderMan")
+// Guest_list.push("IronMan")
+// Guest_list.splice(2,1,"Loki")
+
+// Guest_list.forEach(Guest_list => {
+//     console.log(`Hey ${Guest_list} would you like  dinner with me. i have find bigger table so i am inviting more friends. `)
+// });
+// // Question_2: Shrinking Guest List.
+// while (Guest_list.length > 2) {
+//     let Guest_Removed= Guest_list.pop()
+//     console.log (`unfortunately big table is not available so ${Guest_Removed} your not invited for dinner and sorry.`)
+// }
+// Guest_list.forEach(Guest_list => {
+//     console.log(`your still invited ${Guest_list}. `)
+// })
+// // Question_3: 
+// let places = ["Islamabad","New York","HongKong","Pairs"]
+// console.log("Original order:", places);
+// console.log("Alphabetical order:", [...places].sort());
+// console.log("Original order:", places);
+// console.log("Reverse alphabetical order:", [...places].sort().reverse());
+// console.log("Original order:", places);
+// places.reverse();
+// console.log("Reversed order:", places);
+// places.reverse();
+// console.log("Original order:", places);
+// places.sort();
+// console.log("Sorted in alphabetical order:", places);
+// places.sort((a, b) => b.localeCompare(a));
+// console.log("Sorted in reverse alphabetical order:", places);
+
+// //Day_6 Complete.
