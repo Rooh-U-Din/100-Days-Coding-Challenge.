9316dbf305f71fe20a8cd3ffae7557f6a197f214
Rooh-U-Din <160412491+Rooh-U-Din@users.noreply.github.com>
2024-03-11 04:34:00 -0700

    100-Days-Coding-Challenge Day-7
    100-Days-Coding-Challenge Day-7

---
+++ Day_7.ts
@@ -0,0 +1,20 @@
+// // Question_1: Dinner Guests.
+// let Guest_list=["BatMan","Thor","Wanda","Buckey"]
+// console.log(`I am inviting ${Guest_list.length} guests.`)
+
+// // Question_2: Thinking Of Something.
+
+// let Games = ["Tekken_8","GTA_6","Forza Horizon_5","RDR_2"]
+// console.log(`I would like to play these games :${Games}. in future when i earn myself.`)
+
+// // Question_3: Thinking of something_2
+
+// let kazuya = {
+//     name: "Kazuya Mishima",
+//     fightingStyle: "Mishima Style ",
+//     specialMoves: ["Electric Wind God Fist", "Devil Transformation",],
+//     nationality: "Japanese",
+//   };
+//   console.log(`${kazuya.name} is a tekken character his fighting style is ${kazuya.fightingStyle} and his special moves is ${kazuya.specialMoves} he is from ${kazuya.nationality}.`);
+
+//   // Day_7 Complete.
