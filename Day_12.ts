28f8e911e50ca66d12563dcc60e1209eab5fe4f2
Rooh-U-Din <160412491+Rooh-U-Din@users.noreply.github.com>
2024-03-13 14:44:17 +0500

    100-Days-Coding-Challenge Day-12
    100-Days-Coding-Challenge Day-12

---
+++ Day_12.ts
@@ -0,0 +1,26 @@
+// Question_1:Pizzas
+let pizzas = ["Margherita","Chicken tikka","Fajita"]
+
+pizzas.forEach(pizzas => {
+    console.log(`I love to eat ${pizzas} pizza flavor.`);
+    
+});
+console.log(`I love to eat pizza.`);
+
+// Question_2:Animals
+
+let animals=["Cat","Lion","Leopard","Tiger"]
+
+animals.forEach(animals => {
+    console.log(`The ${animals} a member of the cat family and is a great predator.`);
+    
+});
+console.log("These animals is great predators.");
+
+// Question_3:Tshirt
+
+function make_tshirt(size:string,company:string) {
+    console.log(`tshirt size :${size} , and company is :${company}.`);
+    
+}
+make_tshirt ("small","adidas")
