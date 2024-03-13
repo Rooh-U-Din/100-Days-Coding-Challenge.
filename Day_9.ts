79694f29fb51c13309a6fa63482c8274eb261fcf
Rooh-U-Din <160412491+Rooh-U-Din@users.noreply.github.com>
2024-03-13 14:41:19 +0500

    100-Days challenge Day-9
    100-Days challenge Day-9

---
+++ Day_9.ts
@@ -0,0 +1,37 @@
+// Question_1:Alien Color
+
+
+
+let alien_color="red"
+
+if (alien_color ="red") {
+    console.log("You just kill red alien");
+    
+}
+
+// Question_2:Alien Color#2
+
+let alien= "yellow"
+
+if (alien=="green") {
+    console.log("congratulations You kill green alien mission complete");
+} else {
+    console.log("mission failed you kill yellow alien");
+    
+}
+
+// Question_3:Alien Color#3
+
+let aliens= "green"
+
+if (aliens="red") {
+    console.log("You just killed red alien");
+}else if(aliens="green"){
+    console.log("congratulations You kill green alien mission complete");
+    
+}else{
+    console.log("mission failed you killed wrong alien");
+    
+}
+
+// Day_9 complete
