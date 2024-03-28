c04d4ac61d6ca4a02f660a234b1b45007e06b1b4
Rooh-U-Din <160412491+Rooh-U-Din@users.noreply.github.com>
2024-03-28 17:02:39 +0500

    100-Days-Coding-Challenge-Day-23
    100-Days-Coding-Challenge-Day-23

---
+++ Day_23.ts
@@ -0,0 +1,26 @@
+// //Question_1:Arithmetic Mixed
+
+// function parameters(str:string,num:number) {
+//     return str+num
+// }
+// console.log(parameters("2",78));
+
+// // Question_2:Multiplying Decimals
+
+// function decimal(decimal:number,decimal2:number) {
+//     return Math.round((decimal*decimal2)*100) / 100
+// }
+
+// console.log(decimal(17.33,56.55));
+
+// // Question_3:Dividing And Finding
+
+// function divideWithRemainder(dividend: number, divisor: number): { quotient: number, remainder: number } {
+//     const quotient = Math.floor(dividend / divisor);
+//     const remainder = dividend % divisor;
+//     return { quotient, remainder };
+// }
+
+
+// const result = divideWithRemainder(10, 3);
+// console.log(result); 
