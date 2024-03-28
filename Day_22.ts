1afdfa8f33ded676adbf20b22f3ffb29f5adbd3f
Rooh-U-Din <160412491+Rooh-U-Din@users.noreply.github.com>
2024-03-28 16:56:47 +0500

    100-Days-Coding-Challenge-Day-22
    100-Days-Coding-Challenge-Day-22

---
+++ Day_22.ts
@@ -0,0 +1,25 @@
+// // Question_1:Combine String And Numbers
+
+// let age:number = 1500
+// let Name:string="Thanos is "+age+" Years old."
+// console.log(Name);
+
+// // Question_2:Determine The Remainder
+
+// function remainder(a:number,b:number) {
+//     return console.log(a%b);
+    
+// }
+// remainder(5,2)
+
+// remainder(10,4)
+
+// // Question_3:Logical And Verification
+
+// function Logical(True:boolean,False:boolean) {
+//     return True&&False
+    
+// }
+// console.log(Logical(true,false));
+// console.log(Logical(true,true));
+
