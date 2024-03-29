a99bb5c853d284b20a2ab2e64b15fd21bed0be48
Rooh-U-Din <160412491+Rooh-U-Din@users.noreply.github.com>
2024-03-29 15:55:32 +0500

    100-Days-Coding-Challenge-Day-25
    100-Days-Coding-Challenge-Day-25

---
+++ Day_25.ts
@@ -0,0 +1,34 @@
+// Question_1:Assigning  and Updating Variables
+
+function Name() {
+    let naam = "Fida";
+    console.log(`original name:${naam}`);
+    naam = "Rooh U Din";
+    console.log(`reassigned name:${naam}`);
+    
+}
+Name()
+
+//Question_2:Swapping Variables
+
+function swapping(){
+    let a = 5
+    let b = 10
+    console.log("original values:",a,b);
+    a = 10;
+    b = 5;
+    console.log("swapped values:",a,b);
+    
+}
+swapping()
+//Question_3: Compound Assignment Operators
+let x=4
+x += 2
+console.log("addition:",x);
+x -= 3
+console.log("subtraction:",x);
+x *= 4
+console.log("multiplication:",x);
+x /= 2
+console.log("division:",x);
+
