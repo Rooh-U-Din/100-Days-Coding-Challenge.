b7cc19368cf29369f7fec8a907ee82a579bda30c
Rooh-U-Din <160412491+Rooh-U-Din@users.noreply.github.com>
2024-03-30 13:06:58 +0500

    100-Day-Coding-Challenge-Day26
    100-Day-Coding-Challenge-Day26

---
+++ Day_26.ts
@@ -0,0 +1,27 @@
+// // Question_1:Function Parameters and Return value
+
+// function Adition(Number1:number,Number2:number):number{
+//     return Number1+Number2
+// }
+// console.log(Adition(23,43));
+
+// //Question_2:Default Parameters
+
+// const greet=(Name:string = "Anonymous")=>{
+//     console.log(`Hello ${Name}`);
+// }
+
+// greet("Fida")
+// greet()
+
+// //Question_3:Function Expressions vs Function Declaration
+// const declaration= function (square:number) {
+//     return square*square
+// }
+// console.log(declaration(5));
+
+// function declare(square:number){
+//     console.log(square*square);
+    
+// }
+// declare(5)
