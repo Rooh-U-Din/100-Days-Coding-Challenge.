b01ac6aa69161ed2fdd030bc66ba81cf5de1dacf
Rooh-U-Din <160412491+Rooh-U-Din@users.noreply.github.com>
2024-04-04 22:09:00 +0500

    100-Days-Coding-Challenge-Day-34
    100-Days-Coding-Challenge-Day-34

---
+++ Day_34.ts
@@ -0,0 +1,17 @@
+// Question_1:Square root of 144
+
+let square= Math.sqrt(144)
+console.log(square);
+
+//Question_2: Generate random number between 10-1
+
+let random_number = Math.floor(Math.random()* 10+1);
+console.log(random_number);
+
+//Question_3: Absolute different Number
+
+let different = Math.abs(-5 + -5)
+console.log(different);
+
+
+
