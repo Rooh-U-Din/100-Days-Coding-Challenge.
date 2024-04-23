691233070153e079c531a008e9fecff4e36dcf99
Rooh-U-Din <160412491+Rooh-U-Din@users.noreply.github.com>
2024-04-23 14:28:56 +0500

    100-Days-Coding-Challenge-Day-40
    100-Days-Coding-Challenge-Day-40

---
+++ Day_40.ts
@@ -0,0 +1,23 @@
+// //Question_1:Write a loop that logs numbers from 1 to 10
+
+// for (let number = 1; number < 11; number++) {
+//     const numbers = number
+//     console.log(numbers);
+    
+// }
+
+// //Question_2:Create a While loop that logs "Hello World!" 5 times
+
+// let i=0
+// while (i < 5) {
+//     console.log("Hello World!");
+//     i++;
+// }
+
+// //Question_3:for of loop log favorite movies
+
+// let Movies = ["3Idiots","Batman Dark knight","Avengers Infinity War","Avatar","Zindagi Na Milegi Dubara"];
+
+// for(const Movie of Movies){
+//     console.log(Movie);
+// }
