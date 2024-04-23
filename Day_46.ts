0bb8b9df36a8b532cf4741ae2dd52b0965c6d4bf
Rooh-U-Din <160412491+Rooh-U-Din@users.noreply.github.com>
2024-04-23 14:39:44 +0500

    100-Days-Coding-Challenge-Day-47
    100-Days-Coding-Challenge-Day-47

---
+++ Day_46.ts
@@ -0,0 +1,27 @@
+// // Question_1:Use console.log() to debug and track the value of a variable inside a loop
+
+// for (let i = 0; i <= 5 ; i++) {
+//     console.log(`Iteration ${i}, i value:, i`);
+// }
+
+// // Question_2 Implement a try-catch block to handle potential errors in block of code 
+
+// try {
+    
+//     throw new Error("Something went wrong")
+// } catch (error) {
+//     console.log(error.message);
+// }
+
+// //Question_3:Describe how to use breakpoint in browser developer tools to debug javascript code
+// console.log("Before breakpoint");
+
+// console.log(
+//     "The line has a breakpoint set on it in the browser`s developer tools"
+// );
+
+// console.log("After breakpoint");
+
+
+
+
