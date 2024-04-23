17c5f4e9370898f5bef3c585566a3c5342248cb8
Rooh-U-Din <160412491+Rooh-U-Din@users.noreply.github.com>
2024-04-23 14:30:54 +0500

    100-Days-Coding-Challenge-Day-44
    100-Days-Coding-Challenge-Day-44

---
+++ Day_44.ts
@@ -0,0 +1,23 @@
+// //Question_1:Export function to another file
+// import {ex} from "./function.ts";
+
+// console.log(ex(3,5));
+
+// //Question_2:Export class to another file
+
+// import { greet } from "./class.ts";
+
+// const fida= new greet("fida")
+
+// fida.greet()
+
+// //Question_3:
+
+// import { util_one,util_two } from "./utile.ts";
+
+// util_one("thanos")
+// util_two(1500)
+
+// import calculator from "./class.ts";
+
+// console.log(calculator(3,6));
