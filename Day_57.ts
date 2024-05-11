ae763e514e8882af822dd48e3e4c84e46dcf2f1c
Rooh-U-Din <160412491+Rooh-U-Din@users.noreply.github.com>
2024-05-11 13:20:06 +0500

    100-Days-Coding-Challenge-Day-57
    100-Days-Coding-Challenge-Day-57

---
+++ Day_57.ts
@@ -0,0 +1,35 @@
+let compare = 5>4;
+console.log(compare);
+
+let fruit= "apple">"pineapple";
+console.log(fruit);
+
+let un = undefined==null;
+console.log(un);
+let nul= undefined===null;
+console.log(nul);
+
+let n= null == "\n0\n";
+console.log(n);
+let n2= null==="\n0\n";
+console.log(n2);
+
+
+
+if("0"){
+    console.log("hello");
+}
+
+
+let a:number=1;
+let b:number=2
+let result = (a+b<4)?"Below" : "Over"
+
+console.log(result);
+
+let login = "Employee"
+
+let message =(login == "Employee") ?"Hello":
+(login== "Director") ? "Greeting" : 
+(login =="")? "No login":"";
+console.log(message);
