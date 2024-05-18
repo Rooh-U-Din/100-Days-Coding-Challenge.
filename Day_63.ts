0251cb85dbde5f4638332ee81bd268eea525e74e
Rooh-U-Din <160412491+Rooh-U-Din@users.noreply.github.com>
2024-05-19 00:28:40 +0500

    100-Days-Coding-Challenge-Day-63
    100-Days-Coding-Challenge-Day-63

---
+++ Day_63.ts
@@ -0,0 +1,30 @@
+// Type annotation
+let str:string="Hello World!";
+let nbr:number=42;
+console.log(`My String is ${str}.`);
+console.log(`My Number is ${nbr}.`);
+
+// Another way to write this code is  as follows
+
+let str2:string;
+let nbr2:number;
+
+str2 = "Hello World";
+nbr2=42;
+
+console.log(`My String is ${str2}.`);
+console.log(`My Number is ${nbr2}.`);
+console.log("-------------------------------------------------");
+//Dynamic type determination
+
+let greeting ="Hello World";
+console.log(greeting);
+console.log("-------------------------------------------------");
+
+//Any type
+
+let My_variable:any="Hello World!";
+My_variable=42;
+console.log(My_variable);
+
+
