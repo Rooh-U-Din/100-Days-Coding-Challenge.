2ea588127a207ef6a26ebe7de0af125813aa383e
Rooh-U-Din <160412491+Rooh-U-Din@users.noreply.github.com>
2024-05-25 22:40:35 +0500

    100-Days-Coding-Challenge
    100-Days-Coding-Challenge

---
+++ Day_73.ts
@@ -0,0 +1,6 @@
+// TYPE ASSERTION
+
+
+let data:any="1234";
+
+console.log((data as string).repeat(3));
---
+++ Day_74.ts
@@ -0,0 +1,11 @@
+// DEBUGGING
+
+let age:number=22;
+if(age>20){
+    console.log("Pass");
+}else if(age<20){
+    console.log("Return");
+}else{
+    console.log("Robot");
+    
+}
